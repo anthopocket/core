f781a1b305a8b435b0bdee10c8edfb26ad23fa38
anthopocket <62506538+anthopocket@users.noreply.github.com>
2025-12-31 17:16:15 +0100

    Update const.py

--- homeassistant/components/roborock/const.py
+++ homeassistant/components/roborock/const.py
@@ -60,7 +60,7 @@ GET_VACUUM_CURRENT_POSITION_SERVICE_NAME = "get_vacuum_current_position"
 
 
 A01_UPDATE_INTERVAL = timedelta(minutes=1)
-V1_CLOUD_IN_CLEANING_INTERVAL = timedelta(seconds=30)
-V1_CLOUD_NOT_CLEANING_INTERVAL = timedelta(minutes=1)
-V1_LOCAL_IN_CLEANING_INTERVAL = timedelta(seconds=15)
-V1_LOCAL_NOT_CLEANING_INTERVAL = timedelta(seconds=30)
+V1_CLOUD_IN_CLEANING_INTERVAL = timedelta(seconds=120)
+V1_CLOUD_NOT_CLEANING_INTERVAL = timedelta(minutes=60)
+V1_LOCAL_IN_CLEANING_INTERVAL = timedelta(seconds=120)
+V1_LOCAL_NOT_CLEANING_INTERVAL = timedelta(seconds=60)
